1c489dec4aa4d2addcb3c44163cb45ef0275bc42
dennyWang <thousandwang17@gmail.com>
2023-04-10 10:30:46 +0800

    fix 1. insert to search engine connect stuck , 2. 'front' edit getHost  func

--- backend/encode/insertToSearchEngine/internal/worker/handler/handler.go
+++ backend/encode/insertToSearchEngine/internal/worker/handler/handler.go
@@ -2,7 +2,7 @@
  * @Author: dennyWang thousandwang17@gmail.com
  * @Date: 2023-02-16 12:09:49
  * @LastEditors: dennyWang thousandwang17@gmail.com
- * @LastEditTime: 2023-03-16 19:40:39
+ * @LastEditTime: 2023-04-10 10:26:42
  * @FilePath: /toSearchEngine/internal/worker/handler/handler.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,6 +14,7 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"os"
 	"strings"
 	"sync"
 	"time"
@@ -113,6 +114,7 @@ L:
 
 	}
 	close(h.shutdown)
+	os.Exit(0)
 }
 
 func (h *handle) helper(d []amqp091.Delivery) {
--- front/src/pages/history/axios.js
+++ front/src/pages/history/axios.js
@@ -2,7 +2,7 @@
  * @Author: dennyWang thousandwang17@gmail.com
  * @Date: 2023-03-03 14:15:49
  * @LastEditors: dennyWang thousandwang17@gmail.com
- * @LastEditTime: 2023-04-05 13:57:20
+ * @LastEditTime: 2023-04-10 10:00:11
  * @FilePath: /sidetube/src/common/recommendAxios.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
